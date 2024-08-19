15b8e6530a9b4e2f004f69881ab287520553895b
Mohammed <mrcloud.med@gmail.com>
2024-08-19 16:08:28 +0000

    Fix pycodestyle

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -24,11 +24,13 @@ class Server:
             self.__dataset = dataset[1:]
 
         return self.__dataset
-    
+
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            assert isinstance(page, int) and page > 0
-            assert isinstance(page_size, int) and page_size > 0
-            return self.dataset()[index_range(page, page_size)[0]:index_range(page, page_size)[1]]
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        return self.dataset()[index_range(page, page_size)[0]:index_range(
+            page, page_size)[1]]
+
 
 def index_range(page: int, page_size: int) -> tuple:
     """Return a tuple of size two containing a start index and an end index"""
