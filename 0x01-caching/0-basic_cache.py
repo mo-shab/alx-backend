aa9f2570451723fe15ea3f38763d42a8c3157318
Mohammed <mrcloud.med@gmail.com>
2024-08-20 11:11:28 +0000

    0-basic_cache.py

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Basic cache module"""
+
+
+from base_caching import BaseCaching
+
+class BasicCache(BaseCaching):
+    """ BasicCache class
+        - inherits from BaseCaching and implements a basic caching system
+        - No limit on the number of items stored
+    """
+
+    def put(self, key, item):
+        """ Add an item in the cache
+            If key or item is None, this method should not do anything.
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+            If key is None or if the key doesn’t exist in self.cache_data, return None.
+        """
+        return self.cache_data.get(key)
