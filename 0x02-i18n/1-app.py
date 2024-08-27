c1d4ef047a788b27b947ef828a3ef602ce3cf93d
Mohammed <mrcloud.med@gmail.com>
2024-08-27 22:17:37 +0000

    Task_1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""First app module"""
+
+from flask import Flask
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+if __name__ == "__main__":
+    app.run()
